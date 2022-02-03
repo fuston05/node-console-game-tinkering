a77c197235436ef448db63473604bd8b823498ff
scottfuston <scott.fuston@grandpad.net>
2022-02-02 20:18:40 -0500

    figuring out userInput formatting and action

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const GameBoard = require("./Classes/GameBoard");
 const readline = require("readline");
-const { join } = require("path");
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -21,45 +20,81 @@ gameBoard.buildGrid();
 let player = "X";
 console.log(`Player turn: ${player}`);
 
-const formatCoords = (inp) => {
-  let c = inp.includes(",")
-    ? inp.trim().split(",")
-    : inp.includes(" ")
-    ? inp.trim().split(" ")
-    : inp.trim().split("");
+const formatInput = (inp) => {
+  if (inp.length > 1) {
+    // coords entered
+    let c = inp.includes(",")
+      ? inp.trim().split(",")
+      : inp.includes(" ")
+      ? inp.trim().split(" ")
+      : inp.trim().split("");
 
-  let newC = [];
-  c.forEach((ele) => {
-    if (ele !== "") newC.push(ele.trim());
-  });
-  return newC;
+    let newC = [];
+    c.forEach((ele) => {
+      if (ele !== "") newC.push(ele.trim());
+    });
+    console.log("\ninput: ", newC, "\n");
+    return newC;
+  } else {
+    // quit command
+    let res = inp.trim().toLowerCase();
+    console.log("\ninput: ", res, "\n");
+    return res;
+  }
 };
 
-rl.question("Enter x, y coords (or Q to quit) ", (coords) => {
-  let newC = formatCoords(coords);
+const inputController = (x = null, y = null, q = null) => {
+  // possibilities:
+  // Q to quit
+  // x: 1, 2, 3
+  // y: 1, 2, 3
+  // const userInput = inp;
+  // const switchObj= {
+  //   ['1', '1'] : "",
+  //   ['1', '2'] : "",
+  //   ['1', '3'] : "",
+  //   ['2', '1'] : "",
+  //   ['2', '2'] : "",
+  //   ['2', '3'] : "",
+  //   ['3', '1'] : "",
+  //   ['3', '2'] : "",
+  //   ['3', '3'] : "",
+  // }
+};
 
-  //   number of coords
-  if (newC.length !== 2) {
+rl.question("Enter x, y coords (or Q to quit) ", (userInput) => {
+  let input = formatInput(userInput);
+  console.log("type: ", typeof input);
+
+  let type = typeof input;
+
+  if (type === "string") {
+    console.log("string: ", input);
+    return;
+  }
+
+  //   number of chars/digits
+  if (type === "object" && input.length !== 2) {
     console.log(`Please enter 2 coords: X, Y`);
     return;
   }
 
-  let x = newC[0].trim();
-  let y = newC[1].trim();
-
-  //   number of digits / coord
-  if (y.length > 1 || x.length > 1) {
+  //  length of each digit
+  if ((type === "object" && input[0].length > 1) || input[1].length > 1) {
     console.log(
-      `One of your coords contains more than one digit: X:${x}, Y: ${y}`
+      `One of your coords contains more than one digit: X: ${input[0]}, Y: ${input[1]}`
     );
     return;
   }
 
-  if (!x.match(/\d/g)) {
-    console.log(`must be a number only`);
+  // number only
+  if (type === "object" && !input[0].match(/\d/g)) {
+    console.log(`Numbers only`);
     return;
   }
 
-  console.log("c: ", newC);
-  console.log(`You entered coords: ${x}, ${y}`);
+  if ((type === "object" && input[0] > 3) || input[1] > 3) {
+    console.log(`num < 4 only`);
+    return;
+  }
 });
